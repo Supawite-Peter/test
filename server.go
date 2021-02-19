6ea3bb77642570526743b55c7d6bb9ccbfd23b27
Supawite-Peter <79275392+Supawite-Peter@users.noreply.github.com>
2021-02-19 13:05:00 +0700

    Update server.go

--- server.go
+++ server.go
@@ -18,15 +18,15 @@ func main() {
 	
 	router := gin.Default()
 	
-	router.LoadHTMLFiles("/template/index.tmpl")	
+	router.LoadHTMLFiles("./template/index.tmpl")	
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK,"index.tmpl", nil)
 		//c.HTML(http.StatusOK,"http.tmpl", nil)
 	})
 
-	//router.StaticFS("/static", http.Dir("./static"))
-	router.Static("/static", "static")
+	router.StaticFS("/static", http.Dir("./static"))
+	//router.Static("/static", "static")
 
 	router.Run(":" + port)
 	//router.RunTLS(":8080", "/home/peter/ssl/leaf.pem", "/home/peter/ssl/leaf.key")
