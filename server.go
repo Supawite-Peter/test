c5d586d267c67328cc1f3911e6837193d0b7061b
Supawite-Peter <79275392+Supawite-Peter@users.noreply.github.com>
2021-02-19 12:31:25 +0700

    Update server.go

--- server.go
+++ server.go
@@ -4,12 +4,20 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
 func main() {
+	
+	port := os.Getenv("PORT")
+	
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	
 	router := gin.Default()
 	
-	router.LoadHTMLFiles("./template/index.tmpl")	
+	router.LoadHTMLFiles("/template/index.tmpl")	
 
 	router.GET("/main", func(c *gin.Context) {
 		c.HTML(http.StatusOK,"index.tmpl", nil)
@@ -18,6 +26,6 @@ func main() {
 
 	router.StaticFS("/static", http.Dir("./static"))
 
-	router.Run(":8080")
+	router.Run(":" + port)
 	//router.RunTLS(":8080", "/home/peter/ssl/leaf.pem", "/home/peter/ssl/leaf.key")
 }
